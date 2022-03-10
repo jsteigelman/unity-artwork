3ce0f43e3e151701e43f75f57a1e1e7edfc10710
Joey Steigelman <joey.steigelman@gmail.com>
2022-03-10 16:31:19 -0500

    removed unusued components

--- Assets/DisplayImage.cs
+++ Assets/DisplayImage.cs
@@ -4,36 +4,6 @@ using UnityEngine.Networking;
 using System.Collections.Generic;
 
 public class DisplayImage : MonoBehaviour {
-    // void Start() {
-    //     StartCoroutine(GetTexture());
-    //     Debug.Log("This is running!");
-    // }
- 
-    // IEnumerator GetTexture() {
-    //     UnityWebRequest www = UnityWebRequestTexture.GetTexture("https://images.metmuseum.org/CRDImages/as/original/DP251139.jpg");
-    //     yield return www.SendWebRequest();
-
-    //     if (www.result != UnityWebRequest.Result.Success) {
-    //         Debug.Log(www.error);
-    //     }
-    //     else {
-    //         Texture myTexture = ((DownloadHandlerTexture)www.downloadHandler).texture;
-    //     }
-    // }
-
-    // void Start(){    
-    //     StartCoroutine(DownloadImage("https://images.metmuseum.org/CRDImages/as/original/DP251139.jpg"));
-    // }
-
-    // IEnumerator DownloadImage(string MediaUrl)
-    // {   
-    //     UnityWebRequest request = UnityWebRequestTexture.GetTexture(MediaUrl);
-    //     yield return request.SendWebRequest();
-    //     if(request.isNetworkError || request.isHttpError) 
-    //         Debug.Log(request.error);
-    //     else
-    //         YourRawImage.texture = ((DownloadHandlerTexture) request.downloadHandler).texture;
-    // } 
 
     public string TextureURL = "https://images.metmuseum.org/CRDImages/ep/original/DP-14936-023.jpg";
 
@@ -43,9 +13,6 @@ public class DisplayImage : MonoBehaviour {
         StartCoroutine(DownloadImage(TextureURL));
         Debug.Log("Script is running!");
     }
-
-         
-    // }
  
     IEnumerator DownloadImage(string MediaUrl)
     {
